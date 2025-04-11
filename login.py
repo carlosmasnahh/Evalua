49061146b0f2aff5250329f6528eb120975c7c6b
carlosmasnahh <cv6144428@gmail.com>
2025-04-11 09:47:22 -0500

    Agregada función de registro de usuarios

--- login.py
+++ login.py
@@ -2,3 +2,8 @@
 def validar_usuario(username, password):
     # Lógica de validación
     return True
+
+def registrar_usuario(username, password, email):
+    # Lógica de registro
+    print('Usuario registrado correctamente')
+    return True
